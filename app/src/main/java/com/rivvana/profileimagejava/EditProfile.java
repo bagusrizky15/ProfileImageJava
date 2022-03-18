944d439906af98c39699e2153cfc7921a0947a74
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-03-18 21:17:55 +0700

    .

--- app/src/main/java/com/rivvana/profileimagejava/EditProfile.java
+++ app/src/main/java/com/rivvana/profileimagejava/EditProfile.java
@@ -2,13 +2,72 @@ package com.rivvana.profileimagejava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfile extends AppCompatActivity {
+
+    private CircleImageView profileImage;
+    private Button closeButton, saveButton;
+    private TextView changeProfile;
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
+    private Uri imageUri;
+    private String myUri = "";
+    private StorageTask uploadTask;
+    private StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
+        storageReference = FirebaseStorage.getInstance().getReference().child("Profile Pic");
+
+        profileImage = findViewById(R.id.profleImage);
+        closeButton = findViewById(R.id.closeBtn);
+        saveButton = findViewById(R.id.saveBtn);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditProfile.this, MainActivity.class));
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadProfileImage();
+            }
+        });
+
+        changeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setAspectRatio(1,1).start(EditProfile.this
+                );
+            }
+        });
+
     }
 }
