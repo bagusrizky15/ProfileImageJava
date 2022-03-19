85e861aa3e8c32e00ba5b8f054e335d827bc432a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-03-19 08:00:01 +0700

    m

--- app/src/main/java/com/rivvana/profileimagejava/EditProfile.java
+++ app/src/main/java/com/rivvana/profileimagejava/EditProfile.java
@@ -1,14 +1,21 @@
 package com.rivvana.profileimagejava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,6 +23,8 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -64,10 +73,63 @@ public class EditProfile extends AppCompatActivity {
         changeProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CropImage.activity().setAspectRatio(1,1).start(EditProfile.this
-                );
+                CropImage.activity().setAspectRatio(1,1).start(EditProfile.this);
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+            profileImage.setImageURI(imageUri);
+
+        }else {
+            Toast.makeText(this, "Error, try again!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void uploadProfileImage(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Set your profile");
+        progressDialog.setMessage("Please wait, while we are setting your data ");
+        progressDialog.show();
+
+        if (imageUri != null){
+            final StorageReference fileRef = storageReference
+                    .child(mAuth.getCurrentUser().getUid()+ ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (task.isSuccessful()){
+                        throw task.getException()
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+
+                    if (task.isSuccessful()){
+                        Uri downloadUri = (Uri) task.getResult();
+                        myUri = downloadUri.toString();
+
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("image", myUri);
+
+                        databaseReference.child(mAuth.getCurrentUser().getUid().updateChildren())
+
+                    }
+
+                }
+            });
+        }
     }
 }
